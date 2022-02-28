515664f6333a95dd3eceeb3223b7d5fd8d726445
piyush16108 <100013910+piyush16108@users.noreply.github.com>
2022-02-28 13:13:57 +0530

    Add files via upload

---
+++ font_resize.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	
+	var resize = new Array(".alltext","h3","h5");
+	resize = resize.join(",");
+	$("#increase").click(function(){
+		var curr_font_in_pixels = $(".alltext").css("font-size");
+		var curr_font = parseInt(curr_font_in_pixels);
+		alert(curr_font + 5);
+		$(resize).css("font-size", curr_font + 5);
+	});
+	
+	$("#decrease").click(function(){
+		if($(".alltext").css("font-size")>5){
+			$(".alltext").css("font-size", $(".alltext").css("font-size") - 5);
+		}
+	});
+});
