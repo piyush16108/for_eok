22ab256c1b23676744228e632ad03ca820c9be53
piyush16108 <100013910+piyush16108@users.noreply.github.com>
2022-03-01 21:03:56 +0530

    Update font_resize.js

--- font_resize.js
+++ font_resize.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 	var resize = new Array(".alltext","h3","h5");
 	resize = resize.join(",");
 	$("#increase").click(function(){
+		
 		curr_font_in_pixels = $(".alltext").css("font-size");
 		curr_font = parseInt(curr_font_in_pixels);
 		//alert(curr_font + 10);
@@ -11,9 +12,11 @@ $(document).ready(function(){
 	});
 	
 	$("#decrease").click(function(){
-		if($(".alltext").css("font-size")>10){
-			curr_font_in_pixels = $(".alltext").css("font-size");
-			curr_font = parseInt(curr_font_in_pixels);
+		
+		curr_font_in_pixels = $(".alltext").css("font-size");
+		curr_font = parseInt(curr_font_in_pixels);
+		
+		if(curr_font>10){
 			$(resize).css("font-size", curr_font - 10);
 		}
 	});
