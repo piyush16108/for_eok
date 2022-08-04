5874ba3e57709568e6fab7d88df323d77c81f128
piyush16108 <100013910+piyush16108@users.noreply.github.com>
2022-08-04 20:52:34 +0530

    Update font_resize.js

--- font_resize.js
+++ font_resize.js
@@ -1,14 +1,19 @@
 $(document).ready(function(){
 	var curr_font_in_pixels = 0;
+	var curr_padding_in_pixels = 0;
 	var curr_font = 0;
+	var curr_padding = 0;
 	var resize = new Array(".alltext","h3","h5");
 	resize = resize.join(",");
 	$("#increase").click(function(){
 		
 		curr_font_in_pixels = $(".alltext").css("font-size");
 		curr_font = parseInt(curr_font_in_pixels);
-		alert(curr_font*1.2);
+		curr_padding_in_pixels = $(".alltext").css("padding");
+		curr_padding = parseInt(curr_padding_in_pixels);
+		alert("Font: " + curr_font*1.2 + " Padding: " + curr_padding*1.2);
 		$(resize).css("font-size", curr_font*1.2);
+		$(resize).css("padding", curr_padding*1.2);
 	});
 	
 	$("#decrease").click(function(){
