249ea874d1450f18956acf0cd42c157ffa57b68b
piyush16108 <100013910+piyush16108@users.noreply.github.com>
2022-08-08 20:07:18 +0530

    Update font_resize.js

--- font_resize.js
+++ font_resize.js
@@ -11,14 +11,14 @@ $(document).ready(function(){
 		curr_font = parseInt(curr_font_in_pixels);
 		curr_padding_in_pixels = $(".alltext").css("padding");
 		curr_padding = parseInt(curr_padding_in_pixels);
-		alert("Font: " + curr_font*1.2 + " Padding: " + curr_padding*1.2);
+		//alert("Font: " + curr_font*1.2 + " Padding: " + curr_padding*1.2);
 		$(resize).css("font-size", curr_font*1.2);
 		$(resize).css("padding", curr_padding*1.2);
 	});
 	
 	$("#decrease").click(function(){
 		
-		alert("You wanna decrease");
+		//alert("You wanna decrease");
 		curr_font_in_pixels = $(".alltext").css("font-size");
 		curr_font = parseInt(curr_font_in_pixels);
 		
